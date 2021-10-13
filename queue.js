7b89868ca745ad3be4fee8c8e02b3dcd145b85db
Jerry Hsu <Jerry>
2021-10-13 10:40:23 -0400

    Stacks & Queues main exercises

--- queue.js
+++ queue.js
@@ -4,6 +4,7 @@ class Node {
   constructor(val) {
     this.val = val;
     this.next = null;
+    this.previous = null;
   }
 }
 
@@ -20,26 +21,41 @@ class Queue {
   /** enqueue(val): add new value to end of the queue. Returns undefined. */
 
   enqueue(val) {
-
+    const newNode = new Node(val);
+    if (!this.first){
+      this.first = this.last = newNode;
+    } else {
+      [ this.last.next, newNode.previous ] = [ newNode, this.last];
+      this.last = newNode;
+    }
+    this.size ++;
+    return;
   }
 
   /** dequeue(): remove the node from the start of the queue
    * and return its value. Should throw an error if the queue is empty. */
 
   dequeue() {
-
+    try {
+      const firstNode = this.first;
+      this.first = firstNode.next;
+      this.size --;
+      return firstNode.val;
+    } catch(e) {
+      throw e
+    }
   }
 
   /** peek(): return the value of the first node in the queue. */
 
   peek() {
-
+    return this.first ? this.first.val : null;
   }
 
   /** isEmpty(): return true if the queue is empty, otherwise false */
 
   isEmpty() {
-
+    return this.first === null
   }
 }
 
--- stack.js
+++ stack.js
@@ -20,26 +20,39 @@ class Stack {
   /** push(val): add new value to end of the stack. Returns undefined. */
 
   push(val) {
-
+    const newNode = new Node(val);
+    if (this.first){
+      [ newNode.next, this.first ] = [ this.first, newNode ];
+    } else {
+      this.first = this.last = newNode;
+    }
+    this.size ++;
   }
 
   /** pop(): remove the node from the top of the stack
    * and return its value. Should throw an error if the stack is empty. */
 
   pop() {
-
+    try {
+      const firstNode = this.first;
+      this.first = this.first.next;
+      this.size --;
+      return firstNode.val;
+    } catch(e) {
+      throw e;
+    }
   }
 
   /** peek(): return the value of the first node in the stack. */
 
   peek() {
-
+    return this.first ? this.first.val : null;
   }
 
   /** isEmpty(): return true if the stack is empty, otherwise false */
 
   isEmpty() {
-
+    return this.first === null;
   }
 }
 
